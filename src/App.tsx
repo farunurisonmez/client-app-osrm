f2f913576f4b936803bfb57b17e45ffc96812f8e
Faru Nuri SÖNMEZ <farunuri.sonmez@abclojistik.com>
2023-10-22 16:42:44 +0300

    Rota Oluşturma Entegrasyonu #3
    Başlangıç ve Bitiş noktalarını kullanarak rota bilgisi almak için OSRM Api servisi oluşturuldu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react';
 import ApiNominatimService from "./services/nominatim/nominatim.service"
+import ApiOSRMService from './services/osrm/osrm.service';
 
 function App() {
     useEffect(()=>{
-      ApiNominatimService.fetchNominatim({search:"kayseri"}).then((res)=>{
+      /* ApiNominatimService.fetchNominatim({search:"kayseri"}).then((res)=>{
         console.log(res)
-      })
+      }) */
+      ApiOSRMService.fetchRoute({
+        startingCoordinate: [38, 34],
+        endCoordinate: [38,33]
+      }).then(res=>{
+        console.log(res)
+      }).catch((err)=>console.log(err))
     }, [])
   return (
     <>
